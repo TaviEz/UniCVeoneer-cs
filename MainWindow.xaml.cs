496bf78a3b9bfb3ef3b52090fb90e56d426ea996
TaviEz <tavicoste13@gmail.com>
2023-05-05 16:32:05 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TemaThreadsTasks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Console.ReadLine();
+        }
+
+
+        private void normalExecute_Click(object sender, RoutedEventArgs e)
+        {
+            MainLabel.Content = "Start ";
+            Thread.Sleep(10000);
+            MainLabel.Content += "End ";
+        }
+
+        private async void asyncExecute_Click(object sender, RoutedEventArgs e)
+        {
+            SecondLabel.Content = "Start ";
+            await Task.Delay(10000);
+            SecondLabel.Content += "End";
+        }
+    }
+}
